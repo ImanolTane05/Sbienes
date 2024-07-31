2c77bfb395723847a84e99fd7fbd43a4cb731b0c
Imanol Sánchez <tane05ima@gmail.com>
2024-07-30 22:59:33 -0600

    actualizar datos

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,15 +1,23 @@
-// src/components/EditUser.js
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, getDownloadURL, uploadBytes } from 'firebase/storage';
 import firebaseApp from '../firebase/credenciales';
+import Swal from 'sweetalert2';
+import logo from '../img/logo.png';
+import Pie from '../img/Pie.png';
+import styles from '../styles/adduser.module.css';
+import { FaArrowCircleLeft, FaUserPlus } from 'react-icons/fa'; // Asegúrate de incluir esta línea
 
 const firestore = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
 
 function EditUser() {
   const { id } = useParams();
   const [user, setUser] = useState({});
   const [error, setError] = useState('');
+  const [foto, setFoto] = useState(null);
+  const [fotoUrl, setFotoUrl] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,7 +26,11 @@ function EditUser() {
         const userRef = doc(firestore, `usuarios/${id}`);
         const userSnap = await getDoc(userRef);
         if (userSnap.exists()) {
-          setUser(userSnap.data());
+          const userData = userSnap.data();
+          setUser(userData);
+          if (userData.foto) {
+            setFotoUrl(userData.foto);
+          }
         } else {
           setError('Usuario no encontrado');
         }
@@ -34,10 +46,33 @@ function EditUser() {
     e.preventDefault();
     try {
       const userRef = doc(firestore, `usuarios/${id}`);
+
+      // Actualizar la foto si se ha seleccionado una nueva
+      if (foto) {
+        const fotoRef = ref(storage, `profile_pictures/${id}`);
+        await uploadBytes(fotoRef, foto);
+        const newFotoUrl = await getDownloadURL(fotoRef);
+        user.foto = newFotoUrl;
+      }
+
       await updateDoc(userRef, user);
+      Swal.fire({
+        title: '¡Éxito!',
+        text: 'Usuario actualizado exitosamente.',
+        icon: 'success',
+        confirmButtonColor: '#8A0046',
+        confirmButtonText: 'Aceptar'
+      });
       navigate('/usuarios'); // Redirige a la lista de usuarios después de la edición
     } catch (error) {
       setError('Error actualizando usuario');
+      Swal.fire({
+        title: 'Error',
+        text: 'Hubo un problema al actualizar el usuario. Inténtalo de nuevo.',
+        icon: 'error',
+        confirmButtonColor: '#8A0046',
+        confirmButtonText: 'Aceptar'
+      });
     }
   };
 
@@ -46,56 +81,79 @@ function EditUser() {
     setUser(prevUser => ({ ...prevUser, [name]: value }));
   };
 
+  const handleFotoChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFoto(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFotoUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleBackClick = () => {
+    navigate('/home');
+  };
+
   return (
-    <div>
-      <h1>Editar Usuario</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Nombre:
+    <div className={styles['add-user-panel']}>
+      <header className={styles['add-user-header']}>
+        <img src={logo} alt="Logo" className={styles['logo']} />
+        <h1 className={styles['header-title']}>Editar Usuario</h1>
+        <button className={styles['back-button']} onClick={handleBackClick}>
+          <FaArrowCircleLeft size={20} />
+          Regresar
+        </button>
+      </header>
+      <main className={styles['add-user-content']}>
+        <form onSubmit={handleSubmit} className={styles['add-user-form']}>
+          <div className={styles['photo-upload-container']}>
+            {fotoUrl ? (
+              <img src={fotoUrl} alt="Foto de perfil" className={styles['photo-preview']} />
+            ) : (
+              <FaUserPlus className={styles['fa-user-plus']} />
+            )}
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleFotoChange}
+              className={styles['photo-upload-input']}
+            />
+          </div>
           <input
             type="text"
             name="nombre"
             value={user.nombre || ''}
             onChange={handleChange}
+            placeholder="Nombre"
             required
           />
-        </label>
-        <br />
-        <label>
-          Apellido Paterno:
           <input
             type="text"
             name="apellidoPaterno"
             value={user.apellidoPaterno || ''}
             onChange={handleChange}
+            placeholder="Apellido Paterno"
             required
           />
-        </label>
-        <br />
-        <label>
-          Apellido Materno:
           <input
             type="text"
             name="apellidoMaterno"
             value={user.apellidoMaterno || ''}
             onChange={handleChange}
+            placeholder="Apellido Materno"
             required
           />
-        </label>
-        <br />
-        <label>
-          Correo electrónico:
           <input
             type="email"
             name="email"
             value={user.email || ''}
             onChange={handleChange}
+            placeholder="Correo electrónico"
             required
           />
-        </label>
-        <br />
-        <label>
-          Rol:
           <select
             name="rol"
             value={user.rol || 'user'}
@@ -105,11 +163,13 @@ function EditUser() {
             <option value="admin">Administrador</option>
             <option value="user">Usuario</option>
           </select>
-        </label>
-        <br />
-        <input type="submit" value="Actualizar Usuario" />
-      </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+          <button type="submit" className={styles['add-user-button']}>Actualizar Usuario</button>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+        </form>
+      </main>
+      <footer className={styles['add-user-footer']}>
+        <img src={Pie} alt="Footer Decoration" className={styles['footer-decoration']} />
+      </footer>
     </div>
   );
 }
--- src/components/adduser.js
+++ src/components/adduser.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import firebaseApp from '../firebase/credenciales';
+import { useNavigate } from 'react-router-dom';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { useNavigate } from 'react-router-dom';
+import firebaseApp from '../firebase/credenciales';
+import Swal from 'sweetalert2';
+import logo from '../img/logo.png';
+import Pie from '../img/Pie.png';
+import styles from '../styles/adduser.module.css';
+import { FaArrowCircleLeft, FaUserPlus } from 'react-icons/fa';
 
 const auth = getAuth(firebaseApp);
 const firestore = getFirestore(firebaseApp);
@@ -11,125 +16,159 @@ const storage = getStorage(firebaseApp);
 
 function AddUser() {
   const [nombre, setNombre] = useState('');
-  const [apellidoPaterno, setApellidoPaterno] = useState('');
-  const [apellidoMaterno, setApellidoMaterno] = useState('');
+  const [apellidos, setApellidos] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rol, setRol] = useState('user');
   const [error, setError] = useState('');
   const [foto, setFoto] = useState(null);
+  const [fotoUrl, setFotoUrl] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
 
-    try {
-      // Crear usuario en Firebase Authentication
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
+    const confirm = await Swal.fire({
+      title: '¿Estás seguro?',
+      text: "Se creará un nuevo usuario.",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#8A0046',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, crear',
+      cancelButtonText: 'Cancelar'
+    });
 
-      // Subir foto de perfil a Firebase Storage
-      let fotoUrl = '';
-      if (foto) {
-        const fotoRef = ref(storage, `profile_pictures/${user.uid}`);
-        await uploadBytes(fotoRef, foto);
-        fotoUrl = await getDownloadURL(fotoRef);
+    if (confirm.isConfirmed) {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+
+        let fotoUrl = '';
+        if (foto) {
+          const fotoRef = ref(storage, `profile_pictures/${user.uid}`);
+          await uploadBytes(fotoRef, foto);
+          fotoUrl = await getDownloadURL(fotoRef);
+        }
+
+        const userRef = doc(firestore, `usuarios/${user.uid}`);
+        await setDoc(userRef, {
+          nombre,
+          apellidos,
+          email,
+          rol,
+          foto: fotoUrl,
+        });
+
+        await Swal.fire({
+          title: '¡Éxito!',
+          text: 'Usuario creado exitosamente.',
+          icon: 'success',
+          confirmButtonColor: '#8A0046',
+          confirmButtonText: 'Aceptar'
+        });
+
+        navigate('/usuarios');
+      } catch (error) {
+        console.error('Error creando usuario:', error.message);
+        setError('Error creando usuario. Verifica tus datos e intenta nuevamente.');
+
+        await Swal.fire({
+          title: 'Error',
+          text: 'Hubo un problema al crear el usuario. Verifica tus datos e intenta nuevamente.',
+          icon: 'error',
+          confirmButtonColor: '#8A0046',
+          confirmButtonText: 'Aceptar'
+        });
       }
-
-      // Guardar datos adicionales en Firestore
-      const userRef = doc(firestore, `usuarios/${user.uid}`);
-      await setDoc(userRef, {
-        nombre,
-        apellidoPaterno,
-        apellidoMaterno,
-        email,
-        rol,
-        foto: fotoUrl, // Guarda la URL de la foto en Firestore
-      });
-
-      // Redirigir después de crear el usuario
-      navigate('/usuarios');
-    } catch (error) {
-      console.error('Error creando usuario:', error.message);
-      setError('Error creando usuario. Verifica tus datos e intenta nuevamente.');
     }
   };
 
+  const handleFotoChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFoto(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFotoUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleBackClick = () => {
+    navigate('/home');
+  };
+
   return (
-    <div>
-      <h1>Agregar Usuario</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Nombre:
+    <div className={styles['add-user-panel']}>
+      <header className={styles['add-user-header']}>
+        <img src={logo} alt="Logo" className={styles['logo']} />
+        <h1 className={styles['header-title']}>Agregar Usuario</h1>
+        <button className={styles['back-button']} onClick={handleBackClick}>
+          <FaArrowCircleLeft size={20} />
+          Regresar
+        </button>
+      </header>
+      <main className={styles['add-user-content']}>
+        <form onSubmit={handleSubmit} className={styles['add-user-form']}>
+          <div className={styles['photo-upload-container']}>
+            {fotoUrl ? (
+              <img src={fotoUrl} alt="Foto de perfil" className={styles['photo-preview']} />
+            ) : (
+              <FaUserPlus className={styles['fa-user-plus']} />
+            )}
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleFotoChange}
+              className={styles['photo-upload-input']}
+            />
+          </div>
           <input
             type="text"
             value={nombre}
             onChange={(e) => setNombre(e.target.value)}
+            placeholder="Nombre"
             required
           />
-        </label>
-        <br />
-        <label>
-          Apellido Paterno:
           <input
             type="text"
-            value={apellidoPaterno}
-            onChange={(e) => setApellidoPaterno(e.target.value)}
+            value={apellidos}
+            onChange={(e) => setApellidos(e.target.value)}
+            placeholder="Apellidos"
             required
           />
-        </label>
-        <br />
-        <label>
-          Apellido Materno:
-          <input
-            type="text"
-            value={apellidoMaterno}
-            onChange={(e) => setApellidoMaterno(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Correo electrónico:
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            placeholder="Correo"
             required
           />
-        </label>
-        <br />
-        <label>
-          Contraseña:
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Contraseña"
             required
           />
-        </label>
-        <br />
-        <label>
-          Rol:
-          <select value={rol} onChange={(e) => setRol(e.target.value)} required>
-            <option value="admin">Administrador</option>
+          <select
+            value={rol}
+            onChange={(e) => setRol(e.target.value)}
+            required
+          >
             <option value="user">Usuario</option>
+            <option value="admin">Administrador</option>
           </select>
-        </label>
-        <br />
-        <label>
-          Foto de perfil:
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => setFoto(e.target.files[0])}
-          />
-        </label>
-        <br />
-        <input type="submit" value="Agregar Usuario" />
-      </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+          <button type="submit" className={styles['add-user-button']}>Agregar Usuario <FaUserPlus /></button>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+        </form>
+      </main>
+      <footer className={styles['add-user-footer']}>
+        <img src={Pie} alt="Footer Decoration" className={styles['footer-decoration']} />
+      </footer>
     </div>
   );
 }
